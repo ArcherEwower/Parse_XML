67e4664f0060ba1462f24ada6352b12f47822229
Ewower <ewower12@gmail.com>
2021-08-13 18:10:22 +0600

    6

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const startParse = (url) => {
   fs.readFile(url, (err, data) => {
+    //чтение файла
     parser.parseString(data, function (err, result) {
+      //xml > js object
       console.dir(util.inspect(result, false, null, true));
     });
   });
 };
-let url = path.join(__dirname, "notif_1.xml");
-console.log("1");
-//startParse("notif_1.xml");
+let url = path.join(__dirname, "notif_1.xml"); // путь к файлу
+//startParse("notif_1.xml"); // закуск парсинг xml
 app.use("/api", api);
 const start = async () => {
   try {
